4b080e893c940bf1aafa50bf41149668fd5ec5a8
Mureed Sultan <mureedsultangeni@gmail.com>
2022-10-05 14:51:19 +0500

    first commit

--- src/scripts/app/site/pages/home.js
+++ src/scripts/app/site/pages/home.js
@@ -1,17 +1,10 @@
 import getComp from 'setjs/template/component.js';
-import todo from 'app/site/examples/todo.js';
+// import todo from 'app/site/examples/todo.js';
 
 export default {
   templates: ['site/home'],
-  preload: function({success, error}) {
-    $.get('/templates/todo.html', function(todoTxt) {
-      success(todoTxt);
-   }, 'text').fail(error);
-  },
-  comp: function(opts, todoHtml) {
+  comp: function() {
     var pageComp = getComp('site/home');
-    pageComp.$todo.append(todo.comp().$root);
-    pageComp.$todoHtml[0].innerText = todoHtml;
     return pageComp;
   }
 };
